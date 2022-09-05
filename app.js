510da7c633e12a1631edc99a0cda46b15f5fb9d6
MaximilianoJaraCencosud <maximiliano.jara@cencosud.com>
2022-09-05 17:53:14 -0300

    Creacion del proyecto pagina de mercado liebre

--- app.js
+++ app.js
@@ -2,46 +2,15 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const port = 3030;
+app.use(express.static('public'));
 
 app.listen(port, ()=>{
     console.log(`Escuchando puerto ${port}`);
 })
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/index.html'))
+    res.sendFile(path.join(__dirname, '/views/home.html'))
 });
-
-
-
-app.get('/hamilton', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/hamilton.html'))
-});
-
-app.get('/clarke', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/clarke.html'))
-
-});
-
-
-app.get("/babbage", (req,resp)=>{
-    resp.sendFile(path.join(__dirname,"./views/babbage"))
-});
-
-app.get("/berners-lee", (req,resp)=>{
-    resp.sendFile(path.join(__dirname,"./views/berners-lee"))
-});
-
-app.get('/turing', (requ, resp)=>{
-    resp.sendFile(path.join(__dirname, '/views/turing.html'));
-});
-
-app.get('/lovelace', (requ, resp)=>{
-    resp.sendFile(path.join(__dirname, '/views/lovelace.html'));
-});
-
-app.get('/hopper', (req, res) => {
-    res.sendFile(path.join(__dirname, './views/hopper.html'));
-})
 
 
 
