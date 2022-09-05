94c4c25e0028e16992e24343bce90d5f679b7189
MaximilianoJaraCencosud <maximiliano.jara@cencosud.com>
2022-09-05 12:40:28 -0300

    Creado hopper.html

--- app.js
+++ app.js
@@ -6,3 +6,8 @@ const port = 3030;
 app.listen(port, ()=>{
     console.log(`Escuchando puerto ${port}`);
 })
+
+app.get('/hopper', (req, res) => {
+    res.sendFile(path.join(__dirname, '../views/hopper.html'));
+    })
+
