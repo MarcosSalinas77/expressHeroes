48ecc7262202db4312001d699eefbce305cd2c51
AguAF <agustin.alonso@cencosud.com>
2022-09-05 12:36:37 -0300

    Add archives clarke hamilton

--- app.js
+++ app.js
@@ -6,3 +6,11 @@ const port = 3030;
 app.listen(port, ()=>{
     console.log(`Escuchando puerto ${port}`);
 })
+
+app.get('/hamilton', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/hamilton.html'))
+});
+
+app.get('/clarke', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/clarke.html'))
+});
