fb27c0a4aadd4d037447f295d3e265934a53b1ea
Pablo Martin Clemente <martin.clemente@cencosud.com>
2022-09-05 12:40:55 -0300

    Creado lovelace y turing

--- app.js
+++ app.js
@@ -6,3 +6,12 @@ const port = 3030;
 app.listen(port, ()=>{
     console.log(`Escuchando puerto ${port}`);
 })
+
+
+app.get('/turing', (requ, resp)=>{
+    resp.sendFile(path.join(__dirname, '/views/turing.html'));
+});
+
+app.get('/lovelace', (requ, resp)=>{
+    resp.sendFile(path.join(__dirname, '/views/lovelace.html'));
+});
