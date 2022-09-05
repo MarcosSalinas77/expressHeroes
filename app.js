ed6aece7838b8acdcacdcc14605af4d4f6858467
JoacoVarela <joaquin.varela@cencosud.com>
2022-09-05 12:42:17 -0300

    Parte HTML Joaquin

--- app.js
+++ app.js
@@ -6,3 +6,12 @@ const port = 3030;
 app.listen(port, ()=>{
     console.log(`Escuchando puerto ${port}`);
 })
+
+
+app.get("/babbage", (req,resp)=>{
+    resp.sendFile(path.join(__dirname,"./views/babbage"))
+});
+
+app.get("/babbage", (req,resp)=>{
+    resp.sendFile(path.join(__dirname,"./views/berners-lee"))
+});
