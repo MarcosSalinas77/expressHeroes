a40447d48c4bb69e062a8df5a9ee311975196fdc
MarcosSalinas77 <marcos.cortazar@cencosud.com>
2022-09-05 12:25:56 -0300

    Creando estructura del proyecto

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = 3030;
+
+app.listen(port, ()=>{
+    console.log(`Escuchando puerto ${port}`);
+})
